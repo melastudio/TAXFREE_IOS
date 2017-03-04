129865df8e3da8c3f70021b873378bb36bc2189b
Elżbieta Matus <ela@macbook-air-ela.home>
2017-03-04 13:27:27 +0100

    Initial Commit

---
+++ TAXFREE-app/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  TAXFREE-app
+//
+//  Created by Elżbieta Matus on 04.03.2017.
+//  Copyright © 2017 Elżbieta Matus. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = self.detailItem {
+            if let label = self.detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+
+}
+
---
+++ TAXFREE-appTests/TAXFREE_appTests.swift
@@ -0,0 +1,36 @@
+//
+//  TAXFREE_appTests.swift
+//  TAXFREE-appTests
+//
+//  Created by Elżbieta Matus on 04.03.2017.
+//  Copyright © 2017 Elżbieta Matus. All rights reserved.
+//
+
+import XCTest
+@testable import TAXFREE_app
+
+class TAXFREE_appTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
